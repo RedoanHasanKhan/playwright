b9148ab8993fc7dd3dae7010e21bc23bab423fcd
BS00610 <bs00610@BS00610s-MacBook-Pro.local>
2024-10-10 09:52:32 +0600

    lecture 2.1

---
+++ tests/m2-setup/m3-core-concepts/_1testfunction.test.ts
@@ -0,0 +1,9 @@
+import {test} from '@playwright/test'
+
+test('test title', async() =>{
+
+});
+
+test('test title a', async()=>{
+
+});
---
+++ tests/m2-setup/m3-core-concepts/_2fixures.test.ts
@@ -0,0 +1,6 @@
+import {test, chromium} from '@playwright/test';
+
+test('Test with page fixures', async ({page}) => {
+    await page.goto('https://github.com/RedoanHasanKhan/playwright');
+    console.log("Text content: ", await page.title());
+});
