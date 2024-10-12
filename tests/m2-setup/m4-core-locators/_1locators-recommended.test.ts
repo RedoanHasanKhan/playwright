77aa9e9758052f4f09dea0febd01e4b549ba1c38
BS00610 <bs00610@BS00610s-MacBook-Pro.local>
2024-10-13 01:18:54 +0600

    1st of 4th Chapter

---
+++ tests/m2-setup/m4-core-locators/_1locators-recommended.test.ts
@@ -0,0 +1,12 @@
+import {test, expect} from '@playwright/test';
+
+test('Recommended built in locators examples', async({page})=> {
+    await page.goto('');
+    const firstname = page.getByLabel('First name');
+    await firstname.fill('Sofia');
+    await firstname.clear();
+
+    await page.getByLabel('First name').fill('Andres');
+
+    await page.getByRole('button', {name: 'Register', exact: true}).click();
+});
